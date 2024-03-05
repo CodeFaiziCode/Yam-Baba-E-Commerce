7adf38595d0a825786ddcb717777803e427a1a5a
CodeFaiziCode <CodeFaiziCode@gmail.com>
2024-03-05 11:29:38 +0500

    update

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,10 +1,18 @@
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 import 'package:yam_baba_e_commerce/resources/colors/colors.dart';
 
 class Utils {
   static toastMessage(String message) {
-    Fluttertoast.showToast(msg: message, backgroundColor: AppColor.blackColor);
+    Fluttertoast.showToast(
+        msg: message,
+        backgroundColor: AppColor.blackColor,
+        textColor: AppColor.whiteColor,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM);
   }
 
-  static snackBar(String title, String message) {}
+  static snackBar(String title, String message) {
+    Get.snackbar(title, message);
+  }
 }
