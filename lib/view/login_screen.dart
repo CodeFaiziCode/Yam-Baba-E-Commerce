95f4f84ba575904b38494e4e0267d493c23a139d
CodeFaiziCode <CodeFaiziCode@gmail.com>
2024-03-09 17:36:22 +0500

    update

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:yam_baba_e_commerce/resources/colors/colors.dart';
+import 'package:yam_baba_e_commerce/view_model/login_screen_model.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final loginSM = Get.put(loginScreenModel());
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -10,151 +20,181 @@ class LoginScreen extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      backgroundColor: AppColor.whiteColor, // Set background color as needed
-      body: ListView(
-        padding: EdgeInsets.all(0), // Remove default padding
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        children: [
-          Padding(
-            padding: EdgeInsets.all(screenWidth * 0.05),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: screenHeight * 0.13),
-                Text(
-                  'Welcome Back!',
-                  style: TextStyle(
-                    fontSize: screenWidth * 0.09,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.001),
-                Text(
-                  'Sign in to your account',
-                  style: TextStyle(
-                      fontSize: screenWidth * 0.04, color: AppColor.blackColor),
-                ),
-                SizedBox(height: screenHeight * 0.1),
-                TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: AppColor.fieldColor,
-                    hintText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+      backgroundColor: AppColor.whiteColor,
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            Padding(
+              padding: EdgeInsets.all(screenWidth * 0.05),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: screenHeight * 0.13),
+                  Text(
+                    'Welcome Back!',
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.09,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                SizedBox(height: screenHeight * 0.026),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: AppColor.fieldColor,
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
+                  SizedBox(height: screenHeight * 0.001),
+                  Text(
+                    'Sign in to your account',
+                    style: TextStyle(
+                        fontSize: screenWidth * 0.04,
+                        color: AppColor.blackColor),
                   ),
-                ),
-                SizedBox(height: screenHeight * 0.001),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      // Implement forgot password action
-                    },
-                    child: Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Implement login action
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                      backgroundColor: Colors.black,
-                      minimumSize:
-                          Size(screenWidth * 0.80, screenHeight * 0.07),
-                    ),
-                    child: Text('Login',
-                        style: TextStyle(
-                            fontSize: screenWidth * 0.04,
-                            color: AppColor.whiteColor)),
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                // Divider(),
-                Row(
-                  children: [
-                    Container(
-                        // height: screenHeight * 0.01,
-                        width: screenWidth * 0.2,
-                        child: Divider()),
-                    SizedBox(
-                      width: screenWidth * 0.1,
-                    ),
-                    Text("Or continue with"),
-                    SizedBox(
-                      width: screenWidth * 0.1,
-                    ),
-                    Container(
-                        // height: screenHeight * 0.01,
-                        width: screenWidth * 0.2,
-                        child: Divider()),
-                  ],
-                ),
-                SizedBox(height: screenHeight * 0.01),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: screenWidth * 0.4,
-                      color: Color(0xff316FF6),
-                      child: IconButton(
-                        onPressed: () {
-                          // Implement action for the first social login button
-                        },
-                        icon: Icon(Icons.facebook_outlined),
+                  SizedBox(height: screenHeight * 0.1),
+                  TextFormField(
+                    controller: loginSM.emailController.value,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: AppColor.fieldColor,
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
-                    Container(
-                      width: screenWidth * 0.4,
-                      color: Colors.green,
-                      child: IconButton(
-                        onPressed: () {
-                          // Implement action for the second social login button
-                        },
-                        icon: Icon(Icons.facebook_sharp),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      if (!RegExp(
+                              r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.026),
+                  TextFormField(
+                    controller: loginSM.passwordController.value,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: AppColor.fieldColor,
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: screenHeight * 0.064),
-
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: EdgeInsets.only(bottom: screenHeight * 0.02),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      // Add additional password validation if needed
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: screenHeight * 0.001),
+                  Align(
+                    alignment: Alignment.centerRight,
                     child: TextButton(
                       onPressed: () {
-                        // Implement action for "Not a member? Join Now"
+                        // Implement forgot password action
                       },
-                      child: Text('Not a member? Join Now'),
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.blue),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: screenHeight * 0.03),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Implement login action
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)),
+                        backgroundColor: Colors.black,
+                        minimumSize:
+                            Size(screenWidth * 0.80, screenHeight * 0.07),
+                      ),
+                      child: Text('Login',
+                          style: TextStyle(
+                              fontSize: screenWidth * 0.04,
+                              color: AppColor.whiteColor)),
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.03),
+                  Row(
+                    children: [
+                      Container(
+                        width: screenWidth * 0.2,
+                        child: Divider(),
+                      ),
+                      SizedBox(
+                        width: screenWidth * 0.1,
+                      ),
+                      Text("Or continue with"),
+                      SizedBox(
+                        width: screenWidth * 0.1,
+                      ),
+                      Container(
+                        width: screenWidth * 0.2,
+                        child: Divider(),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: screenWidth * 0.4,
+                        decoration: BoxDecoration(
+                            color: Color(0xff316FF6),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: IconButton(
+                          onPressed: () {
+                            // Implement action for the first social login button
+                          },
+                          icon: Icon(Icons.facebook_outlined),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(10)),
+                        width: screenWidth * 0.4,
+                        child: IconButton(
+                          onPressed: () {
+                            // Implement action for the second social login button
+                          },
+                          icon: Icon(Icons.facebook_sharp),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.064),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: screenHeight * 0.02),
+                      child: TextButton(
+                        onPressed: () {
+                          Get.toNamed('/SignupScreen');
+                          // Implement action for "Not a member? Join Now"
+                        },
+                        child: Text('Not a member? Join Now'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view_model/login_screen_model.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class loginScreenModel extends GetxController {
+  final emailController = TextEditingController().obs;
+  final passwordController = TextEditingController().obs;
+}
