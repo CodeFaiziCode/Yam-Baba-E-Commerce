b5d39119dff9a8447f61b392d7370001636d414a
CodeFaiziCode <CodeFaiziCode@gmail.com>
2024-03-21 16:02:05 +0500

    update

--- lib/view/forgotPasswordScreen/forgotPasswordScreen.dart
+++ lib/view/forgotPasswordScreen/forgotPasswordScreen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:yam_baba_e_commerce/view/forgotPasswordScreen/forgotPasswordScreenModel.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
+import '../../resources/colors/colors.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final forgotEmail = Get.put(ForgotPasswordScreenModel());
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-          // leading: IconButton(
-          //   icon: Icon(Icons.arrow_back),
-          //   onPressed: () {},
-          // ),
-          ),
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -32,26 +41,47 @@ class ForgotPasswordScreen extends StatelessWidget {
             ),
             SizedBox(height: 20),
             TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                border: OutlineInputBorder(),
-              ),
+              controller: forgotEmail.emailController.value,
               keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: AppColor.fieldColor,
+                hintText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Please enter your email';
                 }
-                // You can add more complex email validation here if needed
+                if (!RegExp(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+                    .hasMatch(value)) {
+                  return 'Please enter a valid email address';
+                }
                 return null;
               },
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Action when Send email button is pressed
-              },
-              child: Text('Send Email'),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState != null &&
+                      _formKey.currentState!.validate()) {
+                    // Implement login action
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  backgroundColor: Colors.black,
+                  minimumSize: Size(screenWidth * 0.80, screenHeight * 0.07),
+                ),
+                child: Text('Login',
+                    style: TextStyle(
+                        fontSize: screenWidth * 0.04,
+                        color: AppColor.whiteColor)),
+              ),
             ),
           ],
         ),
