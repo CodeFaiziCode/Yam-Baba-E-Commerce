06b4bb32252af79a90cd93aa01b82f3966967fa5
CodeFaiziCode <CodeFaiziCode@gmail.com>
2024-04-16 20:45:48 +0500

    update

--- lib/resources/components/navigationBar.dart
+++ lib/resources/components/navigationBar.dart
@@ -1,14 +1,59 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';
-import 'package:yam_baba_e_commerce/view/CartScreen/cartScreen.dart';
-import 'package:yam_baba_e_commerce/view/homeScreen/home_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:get/get.dart';
+// import 'package:yam_baba_e_commerce/view/CartScreen/cartScreen.dart';
+// import 'package:yam_baba_e_commerce/view/homeScreen/home_screen.dart';
 
+// import '../../view/CateogoriesScreen/categoriesScreen.dart';
+// import '../../view/ProfileScreen/profileScreen.dart';
+
+// class NavigationMenu extends StatelessWidget {
+//   const NavigationMenu({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = Get.put(NavigationController());
+
+//     return Scaffold(
+//       bottomNavigationBar: Obx(() => NavigationBar(
+//               height: 80,
+//               elevation: 0,
+//               selectedIndex: controller.selectedIndex.value,
+//               onDestinationSelected: (index) =>
+//                   controller.selectedIndex.value = index,
+//               destinations: const [
+//                 NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+//                 NavigationDestination(
+//                     icon: Icon(Icons.category), label: "Category"),
+//                 NavigationDestination(icon: Icon(Icons.trolley), label: "Cart"),
+//                 NavigationDestination(
+//                     icon: Icon(Icons.person), label: "Profile"),
+//               ])),
+//       body: Obx(() => controller.screens[controller.selectedIndex.value]),
+//     );
+//   }
+// }
+
+// class NavigationController extends GetxController {
+//   final Rx<int> selectedIndex = 0.obs;
+//   final screens = [
+//     HomeScreen(),
+//     CategoriesScreen(),
+//     CartScreen(),
+//     ProfileScreen()
+//   ];
+// }
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../view/CartScreen/cartScreen.dart';
 import '../../view/CateogoriesScreen/categoriesScreen.dart';
 import '../../view/ProfileScreen/profileScreen.dart';
+import '../../view/homeScreen/home_screen.dart';
 
 class NavigationMenu extends StatelessWidget {
-  const NavigationMenu({super.key});
+  const NavigationMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +61,20 @@ class NavigationMenu extends StatelessWidget {
 
     return Scaffold(
       bottomNavigationBar: Obx(() => NavigationBar(
-              height: 80,
-              elevation: 0,
-              selectedIndex: controller.selectedIndex.value,
-              onDestinationSelected: (index) =>
-                  controller.selectedIndex.value = index,
-              destinations: const [
-                NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-                NavigationDestination(
-                    icon: Icon(Icons.category), label: "Category"),
-                NavigationDestination(icon: Icon(Icons.trolley), label: "Cart"),
-                NavigationDestination(
-                    icon: Icon(Icons.person), label: "Profile"),
-              ])),
+            height: 80,
+            elevation: 0,
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            destinations: const [
+              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+              NavigationDestination(
+                  icon: Icon(Icons.category), label: "Category"),
+              NavigationDestination(
+                  icon: Icon(Icons.shopping_cart), label: "Cart"),
+              NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+            ],
+          )),
       body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
@@ -40,6 +86,6 @@ class NavigationController extends GetxController {
     HomeScreen(),
     CategoriesScreen(),
     CartScreen(),
-    ProfileScreen()
+    ProfileScreen(),
   ];
 }
--- lib/view/homeScreen/widget/AppDrawer/appDrawer.dart
+++ lib/view/homeScreen/widget/AppDrawer/appDrawer.dart
@@ -1,65 +1,154 @@
+// // import 'package:flutter/material.dart';
+
+// // // Custom Drawer widget
+// // class AppDrawer extends StatelessWidget {
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Drawer(
+// //       child: ListView(
+// //         padding: EdgeInsets.zero,
+// //         children: <Widget>[
+// //           DrawerHeader(
+// //             decoration: BoxDecoration(
+// //               color: Colors.blue,
+// //             ),
+// //             child: Text(
+// //               'Welcome User',
+// //               style: TextStyle(
+// //                 color: Colors.white,
+// //                 fontSize: 24,
+// //               ),
+// //             ),
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.home),
+// //             title: Text('Home'),
+// //             onTap: () {},
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.person),
+// //             title: Text('Profiles'),
+// //             onTap: () {},
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.shopping_cart),
+// //             title: Text('My Orders'),
+// //             onTap: () {},
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.location_on),
+// //             title: Text('Track Your Orders'),
+// //             onTap: () {},
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.help_rounded),
+// //             title: Text('Help Center'),
+// //             onTap: () {},
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.rate_review),
+// //             title: Text('My Reviews'),
+// //             onTap: () {},
+// //           ),
+// //           ListTile(
+// //             leading: Icon(Icons.settings),
+// //             title: Text('Setting'),
+// //             onTap: () {},
+// //           ),
+// //           Spacer(),
+// //           ElevatedButton(
+// //             onPressed: () {},
+// //             child: Text("Log Out"),
+// //           ),
+// //         ],
+// //       ),
+// //     );
+// //   }
+// // }
+
 // import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:yam_baba_e_commerce/resources/components/blackButton.dart';
 
 // // Custom Drawer widget
 // class AppDrawer extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
 //     return Drawer(
-//       child: ListView(
-//         padding: EdgeInsets.zero,
+//       child: Column(
 //         children: <Widget>[
-//           DrawerHeader(
-//             decoration: BoxDecoration(
-//               color: Colors.blue,
+//           Expanded(
+//             child: ListView(
+//               padding: EdgeInsets.zero,
+//               children: <Widget>[
+//                 DrawerHeader(
+//                   decoration: BoxDecoration(
+//                     color: Colors.blue,
+//                   ),
+//                   child: Text(
+//                     'Welcome User',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 24,
+//                     ),
+//                   ),
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.home),
+//                   title: Text('Home'),
+//                   onTap: () {
+//                     Get.toNamed("/NavigationMenu");
+//                     Navigator.pop(context);
+//                   },
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.person),
+//                   title: Text('Profiles'),
+//                   onTap: () {
+//                     Get.toNamed("/NavigationMenu");
+//                     Navigator.pop(context);
+//                   },
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.shopping_cart),
+//                   title: Text('My Orders'),
+//                   onTap: () {},
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.location_on),
+//                   title: Text('Track Your Orders'),
+//                   onTap: () {
+//                     Get.toNamed("/TrackOrderScreen");
+//                     Navigator.pop(context);
+//                   },
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.help_rounded),
+//                   title: Text('Help Center'),
+//                   onTap: () {},
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.rate_review),
+//                   title: Text('My Reviews'),
+//                   onTap: () {},
+//                 ),
+//                 ListTile(
+//                   leading: Icon(Icons.settings),
+//                   title: Text('Setting'),
+//                   onTap: () {},
+//                 ),
+//               ],
 //             ),
-//             child: Text(
-//               'Welcome User',
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: 24,
-//               ),
-//             ),
 //           ),
-//           ListTile(
-//             leading: Icon(Icons.home),
-//             title: Text('Home'),
-//             onTap: () {},
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.person),
-//             title: Text('Profiles'),
-//             onTap: () {},
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.shopping_cart),
-//             title: Text('My Orders'),
-//             onTap: () {},
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.location_on),
-//             title: Text('Track Your Orders'),
-//             onTap: () {},
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.help_rounded),
-//             title: Text('Help Center'),
-//             onTap: () {},
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.rate_review),
-//             title: Text('My Reviews'),
-//             onTap: () {},
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.settings),
-//             title: Text('Setting'),
-//             onTap: () {},
-//           ),
-//           Spacer(),
-//           ElevatedButton(
-//             onPressed: () {},
-//             child: Text("Log Out"),
-//           ),
+//           Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: BlackButton(
+//                 text: "Log out",
+//                 onPressed: () {
+//                   Get.toNamed("/LoginScreen");
+//                 },
+//                 backgroundColor: Colors.black,
+//               ))
 //         ],
 //       ),
 //     );
@@ -70,7 +159,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yam_baba_e_commerce/resources/components/blackButton.dart';
 
-// Custom Drawer widget
+import '../../../../resources/components/navigationBar.dart';
+
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -97,55 +187,71 @@ class AppDrawer extends StatelessWidget {
                   leading: Icon(Icons.home),
                   title: Text('Home'),
                   onTap: () {
-                    Get.toNamed("/HomeScreen");
+                    Get.find<NavigationController>().selectedIndex.value =
+                        0; // Set selected index to HomeScreen (0)
+                    Navigator.pop(context); // Close the drawer
                   },
                 ),
                 ListTile(
                   leading: Icon(Icons.person),
-                  title: Text('Profiles'),
+                  title: Text('Profile'),
                   onTap: () {
-                    Get.toNamed("/ProfileScreen");
+                    Get.find<NavigationController>().selectedIndex.value =
+                        3; // Set selected index to ProfileScreen (3)
+                    Navigator.pop(context); // Close the drawer
                   },
                 ),
                 ListTile(
                   leading: Icon(Icons.shopping_cart),
                   title: Text('My Orders'),
-                  onTap: () {},
+                  onTap: () {
+                    // Handle My Orders onTap behavior here
+                  },
                 ),
                 ListTile(
                   leading: Icon(Icons.location_on),
                   title: Text('Track Your Orders'),
                   onTap: () {
-                    Get.toNamed("/TrackOrderScreen");
+                    Get.toNamed('/TrackOrderScreen');
+
+                    // Navigator.pop(context); // Close the drawer after navigating
                   },
                 ),
                 ListTile(
                   leading: Icon(Icons.help_rounded),
                   title: Text('Help Center'),
-                  onTap: () {},
+                  onTap: () {
+                    // Handle Help Center onTap behavior here
+                  },
                 ),
                 ListTile(
                   leading: Icon(Icons.rate_review),
                   title: Text('My Reviews'),
-                  onTap: () {},
+                  onTap: () {
+                    // Handle My Reviews onTap behavior here
+                  },
                 ),
                 ListTile(
                   leading: Icon(Icons.settings),
                   title: Text('Setting'),
-                  onTap: () {},
+                  onTap: () {
+                    // Handle Setting onTap behavior here
+                  },
                 ),
               ],
             ),
           ),
           Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: BlackButton(
-                text: "Log out",
-                onPressed: () {
-                  Get.toNamed("/LoginScreen");
-                },
-                backgroundColor: Colors.black,
-              ))
+            padding: const EdgeInsets.all(8.0),
+            child: BlackButton(
+              text: "Log out",
+              onPressed: () {
+                Get.toNamed("/LoginScreen");
+                // Navigator.pop(context); // Close the drawer after navigating
+              },
+              backgroundColor: Colors.black,
+            ),
+          ),
         ],
       ),
     );
