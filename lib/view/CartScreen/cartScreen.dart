ab0c358691544568770c837a4d1f19090083d34f
CodeFaiziCode <161572751+CodeFaiziCode@users.noreply.github.com>
2024-05-18 20:31:35 +0500

    Cart screen back button issue resolved

--- lib/view/CartScreen/cartScreen.dart
+++ lib/view/CartScreen/cartScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../homeScreen/widget/AppDrawer/appDrawer.dart';
+import '../../functions/navbarNavigation.dart';
 
 class CartScreen extends StatefulWidget {
   @override
@@ -10,13 +10,24 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          centerTitle: true,
-          title: Text("Cart Screen"),
-        ),
-        drawer: AppDrawer(),
-        body: Column());
+    return WillPopScope(
+      onWillPop: () async {
+        navigateToIndex(0); // Use the dynamic navigation function on back press
+        return false; // Prevent default back button behavior
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                navigateToIndex(0); // Use the dynamic navigation function
+              },
+            ),
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            title: Text("Cart Screen"),
+          ),
+          body: Column()),
+    );
   }
 }
