6ae5f5f2e7dbfc8d399e612be04c01032348313f
CodeFaiziCode <161572751+CodeFaiziCode@users.noreply.github.com>
2024-05-18 17:02:34 +0500

    close app popUp on homeSscreen
    Funtion to close the app on home screen

--- lib/view/homeScreen/home_screen.dart
+++ lib/view/homeScreen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+import 'dart:io';
 
 // Importing specific components
 import '../../resources/components/navigationBar.dart';
@@ -133,209 +134,235 @@ class _HomeScreenState extends State<HomeScreen> {
     ),
   ];
 
+// Funtion to close the app on home screen
+
+  Future<bool> _onWillPop() async {
+    return await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to exit the app?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () => exit(0),
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        flexibleSpace: Container(
-          margin: EdgeInsets.only(
-            top: screenHeight * 0.06,
-            left: screenWidth * 0.056,
-          ),
-          height: screenHeight * 0.048,
-          width: screenWidth * 0.2,
-          child: Center(
-            child: Image.asset(
-              "assets/images/YambabaLogo.png",
-              fit: BoxFit.cover,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          flexibleSpace: Container(
+            margin: EdgeInsets.only(
+              top: screenHeight * 0.06,
+              left: screenWidth * 0.056,
+            ),
+            height: screenHeight * 0.048,
+            width: screenWidth * 0.2,
+            child: Center(
+              child: Image.asset(
+                "assets/images/YambabaLogo.png",
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
-      ),
-      drawer: AppDrawer(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: screenHeight * 0.009),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.042),
-              padding: EdgeInsets.all(screenWidth * 0.009),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(screenWidth * 0.1),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: screenWidth * 0.0000001,
-                    blurRadius: screenWidth * 0.01,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: screenWidth * 0.023),
-                      child: TextField(
-                        style: TextStyle(fontSize: screenWidth * 0.042),
-                        decoration: const InputDecoration(
-                          hintText: '    Search',
-                          border: InputBorder.none,
+        drawer: AppDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: screenHeight * 0.009),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.042),
+                padding: EdgeInsets.all(screenWidth * 0.009),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(screenWidth * 0.1),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: screenWidth * 0.0000001,
+                      blurRadius: screenWidth * 0.01,
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: screenWidth * 0.023),
+                        child: TextField(
+                          style: TextStyle(fontSize: screenWidth * 0.042),
+                          decoration: const InputDecoration(
+                            hintText: '    Search',
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      // Perform search action
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.mic),
-                    onPressed: () {
-                      // Perform voice search action
-                    },
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const FaIcon(FontAwesomeIcons.sliders),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: screenHeight * 0.02),
-            const CarousleSlider(),
-            SizedBox(height: screenHeight * 0.009),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Categories",
-                    style: TextStyle(fontSize: screenHeight * 0.03),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      final controller = Get.find<NavigationController>();
-                      controller.selectedIndex.value = 1;
-                    },
-                    child: Text(
-                      "See All",
-                      style: TextStyle(
-                          fontSize: screenHeight * 0.0175,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blue),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 15.0),
-            const HorizontalCategorySlider(),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Mega Deals",
-                        style: TextStyle(fontSize: screenHeight * 0.03),
-                      ),
-                      const Spacer(),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "See All",
-                          style: TextStyle(
-                              fontSize: screenHeight * 0.0175,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    height: 200,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            ProductCardWidget(
-                              product: products[index],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ), // Space at the bottom of each card
-                          ],
-                        );
+                    IconButton(
+                      icon: const Icon(Icons.search),
+                      onPressed: () {
+                        // Perform search action
                       },
                     ),
-                  ),
-                ],
+                    IconButton(
+                      icon: const Icon(Icons.mic),
+                      onPressed: () {
+                        // Perform voice search action
+                      },
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const FaIcon(FontAwesomeIcons.sliders),
+                    ),
+                  ],
+                ),
               ),
-            ),
+              SizedBox(height: screenHeight * 0.02),
+              const CarousleSlider(),
+              SizedBox(height: screenHeight * 0.009),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Categories",
+                      style: TextStyle(fontSize: screenHeight * 0.03),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        final controller = Get.find<NavigationController>();
+                        controller.selectedIndex.value = 1;
+                      },
+                      child: Text(
+                        "See All",
+                        style: TextStyle(
+                            fontSize: screenHeight * 0.0175,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15.0),
+              const HorizontalCategorySlider(),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Mega Deals",
+                          style: TextStyle(fontSize: screenHeight * 0.03),
+                        ),
+                        const Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "See All",
+                            style: TextStyle(
+                                fontSize: screenHeight * 0.0175,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: products.length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              ProductCardWidget(
+                                product: products[index],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ), // Space at the bottom of each card
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
 
-            // Recommended products section
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Recommended",
-                        style: TextStyle(fontSize: screenHeight * 0.03),
-                      ),
-                      const Spacer(),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "See All",
-                          style: TextStyle(
-                              fontSize: screenHeight * 0.0175,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue),
+              // Recommended products section
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Recommended",
+                          style: TextStyle(fontSize: screenHeight * 0.03),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                    height: 200,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            ProductCardWidget(
-                              product: products[index],
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ), // Space at the bottom of each card
-                          ],
-                        );
-                      },
+                        const Spacer(),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "See All",
+                            style: TextStyle(
+                                fontSize: screenHeight * 0.0175,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: products.length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              ProductCardWidget(
+                                product: products[index],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ), // Space at the bottom of each card
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
