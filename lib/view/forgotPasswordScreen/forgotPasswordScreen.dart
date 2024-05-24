6101b8e52f3130058afe8feb6b09a55e3ecd0d4e
CodeFaiziCode <161572751+CodeFaiziCode@users.noreply.github.com>
2024-05-24 16:46:05 +0500

    Update Forget pass screen in UI

--- lib/view/forgotPasswordScreen/forgotPasswordScreen.dart
+++ lib/view/forgotPasswordScreen/forgotPasswordScreen.dart
@@ -1,100 +1,8 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:yam_baba_e_commerce/view/forgotPasswordScreen/forgotPasswordScreenModel.dart';
-
-// import '../../resources/colors/colors.dart';
-
-// class ForgotPasswordScreen extends StatefulWidget {
-//   @override
-//   State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
-// }
-
-// class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
-//   final forgotEmail = Get.put(ForgotPasswordScreenModel());
-
-//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-//   @override
-//   Widget build(BuildContext context) {
-//     final screenWidth = MediaQuery.of(context).size.width;
-//     final screenHeight = MediaQuery.of(context).size.height;
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Text(
-//               'Forgot Password',
-//               style: TextStyle(
-//                 fontSize: 24,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             SizedBox(height: 10),
-//             Text(
-//               'Please enter your email below to receive your password reset instructions.',
-//               style: TextStyle(
-//                 fontSize: 16,
-//                 color: Colors.grey,
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             TextFormField(
-//               controller: forgotEmail.emailController.value,
-//               keyboardType: TextInputType.emailAddress,
-//               decoration: InputDecoration(
-//                 filled: true,
-//                 fillColor: AppColor.fieldColor,
-//                 hintText: 'Email',
-//                 border: OutlineInputBorder(
-//                   borderRadius: BorderRadius.circular(10.0),
-//                 ),
-//               ),
-//               validator: (value) {
-//                 if (value == null || value.isEmpty) {
-//                   return 'Please enter your email';
-//                 }
-//                 if (!RegExp(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
-//                     .hasMatch(value)) {
-//                   return 'Please enter a valid email address';
-//                 }
-//                 return null;
-//               },
-//             ),
-//             SizedBox(height: 20),
-//             Center(
-//               child: ElevatedButton(
-//                 onPressed: () {
-//                   if (_formKey.currentState != null &&
-//                       _formKey.currentState!.validate()) {
-//                     // Implement login action
-//                   }
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(12)),
-//                   backgroundColor: Colors.black,
-//                   minimumSize: Size(screenWidth * 0.80, screenHeight * 0.07),
-//                 ),
-//                 child: Text('Login',
-//                     style: TextStyle(
-//                         fontSize: screenWidth * 0.04,
-//                         color: AppColor.whiteColor)),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:yam_baba_e_commerce/view/forgotPasswordScreen/forgotPasswordScreenModel.dart';
-
 import '../../resources/colors/colors.dart';
+import '../../resources/components/clipPath.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
@@ -112,77 +20,112 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.all(screenWidth * 0.05),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Forgot Password',
-              style: TextStyle(
-                fontSize: screenWidth * 0.09,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: screenHeight * 0.02),
-            Text(
-              'Please enter your email below to receive your password reset instructions.',
-              style: TextStyle(
-                fontSize: screenWidth * 0.0459,
-                color: Colors.grey,
-              ),
-            ),
-            SizedBox(height: screenHeight * 0.03),
-            TextFormField(
-              controller: forgotEmail.emailController.value,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: AppColor.fieldColor,
-                hintText: 'Email',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(screenWidth * 0.05),
+      backgroundColor: AppColor.whiteColor,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              ClipPath(
+                clipper: CustomClipPath(heightFactor: 1, curveHeight: 60),
+                child: Container(
+                  height: screenHeight * 0.3,
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Image(
+                      image: AssetImage("assets/images/YambabaLogoWhite.png"),
+                      height: 190,
+                    ),
+                  ),
                 ),
               ),
-              style: TextStyle(fontSize: screenWidth * 0.04),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your email';
-                }
-                if (!RegExp(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
-                    .hasMatch(value)) {
-                  return 'Please enter a valid email address';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: screenHeight * 0.03),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Get.toNamed("/OtpScreen");
-
-                  if (_formKey.currentState != null &&
-                      _formKey.currentState!.validate()) {
-                    // Implement login action
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(screenWidth * 0.06)),
-                  backgroundColor: Colors.black,
-                  minimumSize: Size(screenWidth * 0.8, screenHeight * 0.07),
-                ),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                      fontSize: screenWidth * 0.04, color: AppColor.whiteColor),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: screenWidth * 0.05,
+                  right: screenWidth * 0.05,
+                  bottom: screenWidth * 0.05),
+              child: Form(
+                key: _formKey,
+                child: ListView(
+                  children: [
+                    Center(
+                      child: Text(
+                        'Forgot Password',
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.09,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    Center(
+                      child: Text(
+                        'Please enter your email below to receive your password reset instructions.',
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.0459,
+                          color: Colors.grey,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    TextFormField(
+                      controller: forgotEmail.emailController.value,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: AppColor.fieldColor,
+                        hintText: 'Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      style: TextStyle(fontSize: screenWidth * 0.04),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        if (!RegExp(
+                                r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            Get.toNamed("/OtpScreen");
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          backgroundColor: Colors.blue,
+                          minimumSize:
+                              Size(screenWidth * 0.80, screenHeight * 0.07),
+                        ),
+                        child: Text(
+                          'Send',
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.04,
+                            color: AppColor.whiteColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
