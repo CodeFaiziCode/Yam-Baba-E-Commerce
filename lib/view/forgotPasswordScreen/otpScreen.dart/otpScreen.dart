21b4f38d7e9008101f21aee2078b9a24d771d3bc
CodeFaiziCode <161572751+CodeFaiziCode@users.noreply.github.com>
2024-05-24 16:48:56 +0500

    Update OTP screen in UI

--- lib/view/forgotPasswordScreen/otpScreen.dart/otpScreen.dart
+++ lib/view/forgotPasswordScreen/otpScreen.dart/otpScreen.dart
@@ -1,138 +1,7 @@
-// // import 'package:flutter/material.dart';
-
-// // class OTPScreen extends StatefulWidget {
-// //   @override
-// //   _OTPScreenState createState() => _OTPScreenState();
-// // }
-
-// // class _OTPScreenState extends State<OTPScreen> {
-// //   TextEditingController _otpController = TextEditingController();
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         title: Text('OTP Verification'),
-// //       ),
-// //       body: Padding(
-// //         padding: EdgeInsets.all(20.0),
-// //         child: Column(
-// //           mainAxisAlignment: MainAxisAlignment.center,
-// //           children: [
-// //             Text(
-// //               'Enter OTP',
-// //               style: TextStyle(
-// //                 fontSize: 20.0,
-// //                 fontWeight: FontWeight.bold,
-// //               ),
-// //             ),
-// //             SizedBox(height: 20.0),
-// //             TextField(
-// //               controller: _otpController,
-// //               keyboardType: TextInputType.number,
-// //               decoration: InputDecoration(
-// //                 labelText: 'OTP',
-// //                 hintText: 'Enter OTP',
-// //                 border: OutlineInputBorder(),
-// //               ),
-// //             ),
-// //             SizedBox(height: 20.0),
-// //             ElevatedButton(
-// //               onPressed: () {
-// //                 // Add logic to verify OTP
-// //                 // For now, print the entered OTP
-// //                 // print('Entered OTP: ${_otpController.text}');
-// //               },
-// //               child: Text('Verify OTP'),
-// //             ),
-// //           ],
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
-
-// import 'package:flutter/material.dart';
-
-// class OTPScreen extends StatefulWidget {
-//   @override
-//   _OTPScreenState createState() => _OTPScreenState();
-// }
-
-// class _OTPScreenState extends State<OTPScreen> {
-//   late List<TextEditingController> _otpControllers;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _otpControllers = List.generate(4, (_) => TextEditingController());
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('OTP Verification'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(20.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               'Enter OTP',
-//               style: TextStyle(
-//                 fontSize: 20.0,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             SizedBox(height: 20.0),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: List.generate(
-//                 4,
-//                 (index) => SizedBox(
-//                   width: 50.0,
-//                   child: TextField(
-//                     controller: _otpControllers[index],
-//                     keyboardType: TextInputType.number,
-//                     maxLength: 1,
-//                     textAlign: TextAlign.center,
-//                     decoration: InputDecoration(
-//                       counterText: '',
-//                       border: OutlineInputBorder(),
-//                     ),
-//                     onChanged: (value) {
-//                       if (value.isNotEmpty) {
-//                         if (index < 3) {
-//                           FocusScope.of(context).nextFocus();
-//                         }
-//                       }
-//                     },
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 20.0),
-//             ElevatedButton(
-//               onPressed: () {
-//                 String otp = _otpControllers
-//                     .map((controller) => controller.text)
-//                     .join('');
-//                 // Add logic to verify OTP
-//                 print('Entered OTP: $otp');
-//               },
-//               child: Text('Verify OTP'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yam_baba_e_commerce/resources/colors/colors.dart';
+import '../../../resources/components/clipPath.dart';
 
 class OTPScreen extends StatefulWidget {
   @override
@@ -153,87 +22,122 @@ class _OTPScreenState extends State<OTPScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('OTP Verification'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Enter OTP',
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                4,
-                (index) => SizedBox(
-                  width: 50.0,
-                  child: TextField(
-                    controller: _otpControllers[index],
-                    keyboardType: TextInputType.number,
-                    maxLength: 1,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      counterText: '',
-                      border: OutlineInputBorder(),
+      backgroundColor: AppColor.whiteColor,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              ClipPath(
+                clipper: CustomClipPath(heightFactor: 1, curveHeight: 60),
+                child: Container(
+                  height: screenHeight * 0.3,
+                  color: Colors.blue,
+                  child: const Center(
+                    child: Image(
+                      image: AssetImage("assets/images/YambabaLogoWhite.png"),
+                      height: 190,
                     ),
-                    onChanged: (value) {
-                      if (value.isNotEmpty) {
-                        if (index < 3) {
-                          FocusScope.of(context).nextFocus();
-                        }
-                      }
-                    },
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 50.0),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // String otp = _otpControllers
-                  //     .map((controller) => controller.text)
-                  //     .join('');
-                  // // Add logic to verify OTP
-                  // print('Entered OTP: $otp');
-
-                  Get.toNamed("/ResetScreen");
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  backgroundColor: Colors.black,
-                  minimumSize: Size(screenWidth * 0.80, screenHeight * 0.07),
-                ),
-                child: Text(
-                  'Verify OTP',
-                  style: TextStyle(
-                    fontSize: screenWidth * 0.04,
-                    color: Colors.white, // Change to white color
-                  ),
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(
+                    left: screenWidth * 0.05,
+                    right: screenWidth * 0.05,
+                    bottom: screenWidth * 0.05),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Enter OTP',
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.09,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                        4,
+                        (index) => SizedBox(
+                          width: screenWidth * 0.15,
+                          child: TextField(
+                            controller: _otpControllers[index],
+                            keyboardType: TextInputType.number,
+                            maxLength: 1,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              counterText: '',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              filled: true,
+                              fillColor: AppColor.fieldColor,
+                            ),
+                            onChanged: (value) {
+                              if (value.isNotEmpty) {
+                                if (index < 3) {
+                                  FocusScope.of(context).nextFocus();
+                                }
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.05),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_otpControllers.every(
+                              (controller) => controller.text.isNotEmpty)) {
+                            // Verify OTP logic here
+                            Get.toNamed("/ResetScreen");
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          backgroundColor: Colors.blue,
+                          minimumSize:
+                              Size(screenWidth * 0.80, screenHeight * 0.07),
+                        ),
+                        child: Text(
+                          'Verify OTP',
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.04,
+                            color: AppColor.whiteColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.05),
+                    TextButton(
+                      onPressed: () {
+                        // Implement logic to resend OTP
+                        print('Resend OTP');
+                      },
+                      child: const Text(
+                        'Didn\'t receive code? Resend it',
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-
-            SizedBox(height: 100), // Added SizedBox for spacing
-            TextButton(
-              onPressed: () {
-                // Implement logic to resend OTP
-                print('Resend OTP');
-              },
-              child: Text('Didn\'t receive code? Resend it'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
